ace2778f872d532e0ce82d7c5603d41fdc166707
Lutvi D Adi <basyukiki@gmail.com>
2025-05-27 23:50:40 +0700

    Create banner.js

---
+++ config/banner.js
@@ -0,0 +1,13 @@
+const banner = [
+    '\nooooooooo       oooooo    oooooo   oooooo     oooo oooooooooooo ooooooooooooo', 
+    '\n 888    Y88   d8P    Y8b    888      888       8    888       8 8    888    8', 
+    '\n 888    d88  888      888    888     8888     8     888              888     ', 
+    '\n 888ooo88P   888      888     888   8  888   8      888oooo8         888     ', 
+    '\n 888 88b     888      888      888 8    888 8       888              888     ', 
+    '\n 888   88b    88b    d88        888      888        888       o      888     ', 
+    '\no888o  o888o   Y8bood8P          8        8        o888ooooood8     o888o    ',                                                                                                                                                                                                                                    
+]
+
+module.exports = {
+    banner
+};
